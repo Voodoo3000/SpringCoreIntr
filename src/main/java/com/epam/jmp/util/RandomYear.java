fb8cd8d02f2af40b73adc2422e010468dbb691fd
Vladislav_Tsay <Vladislav_Tsay@epam.com>
2018-11-23 17:04:54 +0600

    SpEL commit

--- src/main/java/com/epam/jmp/util/RandomYear.java
+++ src/main/java/com/epam/jmp/util/RandomYear.java
@@ -2,14 +2,32 @@ package com.epam.jmp.util;
 
 public class RandomYear {
     private int year;
+    private int startPeriod;
+    private int endPeriod;
 
     public int getYear() {
-        year = randBetween(2013, 2018);
+        year = randBetween(startPeriod, endPeriod);
         return year;
     }
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public int getStartPeriod() {
+        return startPeriod;
+    }
+
+    public void setStartPeriod(int startPeriod) {
+        this.startPeriod = startPeriod;
+    }
+
+    public int getEndPeriod() {
+        return endPeriod;
+    }
+
+    public void setEndPeriod(int endPeriod) {
+        this.endPeriod = endPeriod;
     }
 
     /*
